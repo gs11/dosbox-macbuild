84116193ec622b97ca89719a4d137c4d1caf208a
gs11 <gustav.sinder@gmail.com>
2017-11-11 15:36:52 +0100

    Added App build

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # dosbox-macbuild
-Script to build DOSBox from SVN including building dependencies
+Script to build DOSBox App from SVN including build dependencies
 
 Description
 -----------
@@ -10,3 +10,5 @@ Downloads & builds
 - DOSBox
 
 All dependencies are sandboxed to build folder
+
+Credits to Hexeract for the build instructions https://hexeract.wordpress.com/2016/09/10/building-dosbox-as-x64-binary-for-macos-sierra/
--- buildDosbox.sh
+++ buildDosbox.sh
@@ -1,53 +1,68 @@
 #!/bin/sh
 set -e
 
-BUILD_DIR=`mktemp -d dosbox-build.XXX`
+BUILD_DIR=`pwd`/`mktemp -d dosbox-build.XXX`
+COPYRIGHT_TEXT="Copyright 2002-`date +'%Y'` The DOSBox Team, compiled by $USER"
 
 cd $BUILD_DIR
-mkdir installed
+mkdir dependencies
 
-INSTALL_DIR=`pwd`/installed
-PATH=$PATH:$INSTALL_DIR/bin
+DEPENDENCIES_DIR=$BUILD_DIR/dependencies
+PATH=$PATH:$DEPENDENCIES_DIR/bin
 
 # Autoconf
 curl -LOs http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 tar xzpf autoconf-2.69.tar.gz
 cd autoconf-2.69
-./configure --prefix=$INSTALL_DIR
+./configure --prefix=$DEPENDENCIES_DIR
 make
 make install
-cd ..
-rm -rf autoconf-2.69 autoconf-2.69.tar.gz
+cd $BUILD_DIR
 
 # Automake
 curl -LOs http://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz
 tar xzpf automake-1.15.tar.gz
 cd automake-1.15
-./configure --prefix=$INSTALL_DIR
+./configure --prefix=$DEPENDENCIES_DIR
 make
 make install
-cd ..
-rm -rf automake-1.15 automake-1.15.tar.gz
+cd $BUILD_DIR
 
 # SDL
 curl -LOs https://www.libsdl.org/release/SDL-1.2.15.tar.gz
 tar xzpf SDL-1.2.15.tar.gz
 cd SDL-1.2.15
-./configure --prefix=$INSTALL_DIR --enable-static --disable-shared --disable-video-x11
+./configure --prefix=$DEPENDENCIES_DIR --enable-static --disable-shared --disable-video-x11
 sed -i "" "/CGDirectPaletteRef palette;/d" src/video/quartz/SDL_QuartzVideo.h
 make
 make install
-cd ..
-rm -rf SDL-1.2.15 SDL-1.2.15.tar.gz
+cd $BUILD_DIR
 
 # Dosbox
-svn checkout svn://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk dosbox
-cd dosbox
+svn checkout svn://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk dosbox-src
+cd dosbox-src
 DOSBOXVERSION=$(svn log | head -2 | awk '/^r/ { print $1 }')
 ./autogen.sh
-./configure --with-sdl-prefix=INSTALL_DIR
+./configure --with-sdl-prefix=DEPENDENCIES_DIR
 make
-mv src/dosbox ../dosbox-$DOSBOXVERSION
+mv src/dosbox $BUILD_DIR/dosbox
+cd $BUILD_DIR
 
-cd ..
-rm -rf installed dosbox
+# Create App
+curl -Ls -o DOSBox-0.74-1.dmg https://sourceforge.net/projects/dosbox/files/dosbox/0.74/DOSBox-0.74-1_Universal.dmg/download
+hdiutil attach DOSBox-0.74-1.dmg -quiet
+cp -R /Volumes/DOSBox\ 0.74-1/DOSBox.app .
+hdiutil detach /Volumes/DOSBox\ 0.74-1/ -quiet
+mv dosbox DOSBox.app/Contents/MacOS/DOSBox
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleGetInfoString ${DOSBOXVERSION}, $COPYRIGHT_TEXT" DOSBox.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $DOSBOXVERSION" DOSBox.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $DOSBOXVERSION" DOSBox.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Set :NSHumanReadableCopyright $COPYRIGHT_TEXT" DOSBox.app/Contents/Info.plist
+
+# Cleanup
+rm -rf autoconf-2.69 autoconf-2.69.tar.gz
+rm -rf automake-1.15 automake-1.15.tar.gz
+rm -rf SDL-1.2.15 SDL-1.2.15.tar.gz
+rm -rf dependencies dosbox-src
+rm DOSBox-0.74-1.dmg
